0e38984d8f010155068c38af35c7ef9f22055c79
Navenesvar_27 <navenesvar.raj@multicorewareinc.com>
2024-12-17 22:53:42 +0530

    files added

---
+++ matmul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+int** allocate_matrix(int rows, int cols) {
+    int** matrix = (int**)malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = (int*)malloc(cols * sizeof(int));
+    }
+    return matrix;
+}
+void free_matrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+int** matrix_multiplication(int** A, int** B, int rowsA, int colsA, int colsB) {
+    int** result = allocate_matrix(rowsA, colsB);
+
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < colsB; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < colsA; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return result;
+}
+void print_matrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
